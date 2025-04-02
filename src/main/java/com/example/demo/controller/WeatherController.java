54286208e5e918528205480a8826f56728e5f720
akhilramula <akhilramula38@gmail.com>
2025-04-02 19:40:40 +0530

    Project Added

---
+++ src/main/java/com/example/demo/controller/WeatherController.java
@@ -0,0 +1,41 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
+
+import com.example.demo.entity.WeatherResponse;
+
+@Controller
+public class WeatherController {
+
+	@Value("${api.key}")
+	private String apiKey;
+
+	@GetMapping("/")
+	public String getIndex() {
+		return "index";
+	}
+
+	@GetMapping("/getWeather")
+	public String getWeather(@RequestParam("city") String city, Model model) {
+		String url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=metric";
+		RestTemplate restTemplate = new RestTemplate();
+		WeatherResponse weatherResponse = restTemplate.getForObject(url, WeatherResponse.class);
+		
+		if (weatherResponse != null) {
+			model.addAttribute("city", weatherResponse.getName());
+			model.addAttribute("country", weatherResponse.getSys().getCountry());
+			model.addAttribute("temperature", weatherResponse.getMain().getTemp());
+			model.addAttribute("humidity", weatherResponse.getMain().getHumidity());
+			model.addAttribute("windSpeed", weatherResponse.getWind().getSpeed());
+			model.addAttribute("response", weatherResponse.getWeather().get(0).getDescription());
+		} else {
+			model.addAttribute("error", "City Not Found.");
+		}
+		return "index";
+	}
+}
---
+++ src/main/java/com/example/demo/entity/WeatherResponse.java
@@ -0,0 +1,37 @@
+package com.example.demo.entity;
+
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class WeatherResponse {
+
+	private String name;
+	private Sys sys;
+	private List<Weather> weather;
+	private Main main;
+	private Wind wind;
+
+	@Data
+	public static class Sys {
+		private String country;
+	}
+
+	@Data
+	public static class Weather {
+		private int id;
+		private String description;
+	}
+
+	@Data
+	public static class Main {
+		private double temp;
+		private int humidity;
+	}
+
+	@Data
+	public static class Wind {
+		private double speed;
+	}
+}
